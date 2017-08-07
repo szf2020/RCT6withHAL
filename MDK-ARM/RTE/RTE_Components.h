bc294cbf64a1e009a1133c3039264f4c4e7eb656
XTH <502157064@qq.com>
2017-08-07 15:41:18 +0800

    实现串口发送

---
+++ MDK-ARM/RTE/RTE_Components.h
@@ -0,0 +1,20 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'RCT6' 
+ * Target:  'RCT6' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f10x.h"
+
+
+#endif /* RTE_COMPONENTS_H */
