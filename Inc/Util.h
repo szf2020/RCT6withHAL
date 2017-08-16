c7a4e25273efe4e72130fd6d0f0c103dbb7b883f
XTH <502157064@qq.com>
2017-08-16 17:46:46 +0800

    添加一些实用函数

--- Inc/Util.h
+++ Inc/Util.h
@@ -1,5 +1,12 @@
 #ifndef _UTIL_H_
 #define _UTIL_H_
 #include "stm32f1xx_hal.h"
+#define	_mi(x)					memset((void *)(x), 0, sizeof(*(x)))
+#define	_si(x)					memset((x), 0, sizeof((x)))
+#define	_bit(bit)					(1 << (bit))
+#define	bit_set(n, bit)			((n) |= _bit(bit))
+#define	bit_del(n, bit)			((n) &= (~(_bit(bit))))
+#define	bit_is_set(n, bit)			((n) & _bit(bit))
+
 BOOLEAN bufferCmp(uint8_t* pBuffer1, uint8_t* pBuffer2, uint16_t BufferLength);
 #endif
